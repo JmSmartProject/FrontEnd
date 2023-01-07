2303de1c7b84dc897839130ec416e30fdd2a3c54
JaeKyuP <106376249+JaeKyuP@users.noreply.github.com>
2023-01-07 14:29:02 +0900

    Update profile_settings_page.dart

--- JMSmart_project/lib/modules/profile_page/profile_settings_page.dart
+++ JMSmart_project/lib/modules/profile_page/profile_settings_page.dart
@@ -1,9 +1,368 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-class ProfileSettingsPage extends StatelessWidget{
+class ProfileSettingsPage extends StatefulWidget {
+  @override
+  _ProfileSettingsState createState() => _ProfileSettingsState();
+}
+
+enum Gender {
+  MAN, WOMAN
+}
+
+class _ProfileSettingsState extends State<ProfileSettingsPage>{
+  Gender _gender = Gender.MAN;
+  final IDController = TextEditingController();
+  final PWController = TextEditingController();
+
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.grey.shade50,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: 30,right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                  height: size.height * 0.03
+              ),
+              Text(
+                "나의 정보 수정",
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.w900),
+              ),
+              SizedBox(
+                  height: size.height * 0.02
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.network(
+                          'https://placeimg.com/200/100/people',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.grey[100],
+                        ),
+                        child: Icon(
+                          Icons.camera_alt_outlined,
+                          size: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                  height: size.height * 0.04
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 45,
+                    width: 245,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "닉네임을 입력해주세요",
+                        contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                        labelStyle: TextStyle(fontSize: 14, color: Colors.grey.shade800),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey.shade400),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      width: size.width * 0.02
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    height: 45,
+                    width: 80,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                          )
+                      ),
+                      child: const Text(
+                        "중복 확인",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                  height: size.height * 0.01
+              ),
+              Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "이름을 입력해주세요",
+                      contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                      labelStyle: TextStyle(fontSize: 14,color: Colors.grey.shade800),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                  height: size.height * 0.01
+              ),
+              Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "비밀번호를 입력해주세요",
+                      contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                      labelStyle: TextStyle(fontSize: 14,color: Colors.grey.shade800),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    ),
+                    controller: PWController,
+                  ),
+                ],
+              ),
+              SizedBox(
+                  height: size.height * 0.01
+              ),
+              Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "비밀번호를 한번 더 입력해주세요",
+                      contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                      labelStyle: TextStyle(fontSize: 14,color: Colors.grey.shade800),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.red),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    ),
+                    controller: PWController,
+                  ),
+                ],
+              ),
+              SizedBox(
+                  height: size.height * 0.01
+              ),
+              Text("   전화번호를 입력해주세요",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w500),),
+              SizedBox(
+                  height: size.height * 0.001
+              ),
+              Row(
+                children: <Widget>[
+                  SizedBox(
+                    height: 35,
+                    width: 55,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "010",
+                        contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                        labelStyle: TextStyle(fontSize: 14, color: Colors.grey.shade800),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey.shade400),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      ),
+                    ),
+                  ),
+                  Text("   -   ",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w500),),
+                  SizedBox(
+                    height: 35,
+                    width: 70,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "",
+                        contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                        labelStyle: TextStyle(fontSize: 14, color: Colors.grey.shade800),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey.shade400),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      ),
+                    ),
+                  ),
+                  Text("   -   ",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w500),),
+                  SizedBox(
+                    height: 35,
+                    width: 70,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "",
+                        contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                        labelStyle: TextStyle(fontSize: 14, color: Colors.grey.shade800),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey.shade400),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                  height: size.height * 0.01
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "생년월일을 입력해주세요(8자리)",
+                  contentPadding: EdgeInsets.fromLTRB(15, 10, 10, 10),
+                  labelStyle:
+                      TextStyle(fontSize: 14, color: Colors.grey.shade800),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                ),
+              ),
+              Row(children: <Widget>[
+                Container(
+                  width: size.width * 0.3,
+                  child: RadioListTile(
+                      title: Text("남자"),
+                      value: Gender.MAN,
+                      groupValue: _gender,
+                      onChanged: (value) {
+                        setState(() {
+                          _gender = value!;
+                        });
+                      }),
+                ),
+                Container(
+                  width: size.width * 0.3,
+                  child: RadioListTile(
+                      title: Text("여자"),
+                      value: Gender.MAN,
+                      groupValue: _gender,
+                      onChanged: (value) {
+                        setState(() {
+                          _gender = value!;
+                        });
+                      }),
+                ),
+              ]),
+              SizedBox(
+                  height: size.height * 0.03
+              ),
+              Container(
+                height: 60,
+                child: TextButton(
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      )
+                  ),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                          Color(0xffec407a),
+                          Color(0xfff06292),
+                          Color(0xfff48fb1),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            constraints: BoxConstraints(maxWidth: double.infinity,minHeight: 100),
+                            child: Text("저장",style: TextStyle(fontSize: 16, color: Colors.black,fontWeight: FontWeight.bold),),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
