b8b07ab4aa023572d0f6490d956da37f2e487086
JaeKyuP <106376249+JaeKyuP@users.noreply.github.com>
2023-01-09 19:11:05 +0900

    Update walking_page.dart

--- JMSmart_project/lib/modules/walking_page/walking_page.dart
+++ JMSmart_project/lib/modules/walking_page/walking_page.dart
@@ -8,18 +8,77 @@ class WalkingPage extends StatefulWidget{
 
 class _WalkingPage extends State<WalkingPage> {
   @override
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return MaterialApp(
-      title: 'chat used firebase',
-      theme: ThemeData(primaryColor: Colors.white),
       home: Scaffold(
-        body: Container(
-          color: Color(0xfff48fb1),
+        backgroundColor: Color(0xfff48fb1),
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: size.height * 0.03),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "산책 파트너 매칭",
+                      style:
+                      TextStyle(fontSize: 27, fontWeight: FontWeight.w900),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.02,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      width: 70,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: const Text(
+                          "산책 관리",
+                          style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.01,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      width: 70,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: const Text(
+                          "매칭 설정",
+                          style: TextStyle(
+                              fontSize: 10,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
