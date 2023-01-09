06aefb8b3871c7a701be6fd3c101dd1e3b165960
JaeKyuP <106376249+JaeKyuP@users.noreply.github.com>
2023-01-09 19:08:10 +0900

    Update chatting_page.dart

--- JMSmart_project/lib/modules/chatting_page/chatting_page.dart
+++ JMSmart_project/lib/modules/chatting_page/chatting_page.dart
@@ -8,14 +8,74 @@ class ChattingPage extends StatefulWidget{
 
 class _ChattingPage extends State<ChattingPage> {
   @override
-
   Widget build(BuildContext context){
+    Size size = MediaQuery.of(context).size;
     return MaterialApp(
-      title: 'chat used firebase',
-      theme: ThemeData(primaryColor: Colors.white),
       home: Scaffold(
-        body: Container(
-          color: Colors.red,
+        backgroundColor: Colors.deepOrangeAccent,
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: size.height * 0.03),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "채팅",
+                      style:
+                      TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.3,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      width: 60,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: const Text(
+                          "친구",
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.01,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 30,
+                      width: 60,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        child: const Text(
+                          "설정",
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
