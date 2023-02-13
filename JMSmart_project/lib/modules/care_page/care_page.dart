fe89f5d49f38e99a534245def29692ca95b7e8db
Jeagu <106376249+JaeKyuP@users.noreply.github.com>
2023-02-13 17:38:17 +0900

    Update care_page.dart

--- JMSmart_project/lib/modules/care_page/care_page.dart
+++ JMSmart_project/lib/modules/care_page/care_page.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class CarePage extends StatefulWidget{
+class CarePage extends StatefulWidget {
   @override
-  _CarePage createState() => _CarePage();
+  _CarePageState createState() => _CarePageState();
 }
 
-class _CarePage extends State<CarePage> {
-  @override
+class _CarePageState extends State<CarePage> {
+  late List<_ChartData> data;
+  late TooltipBehavior _tooltip;
 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'chat used firebase',
-      theme: ThemeData(primaryColor: Colors.white),
-      home: Scaffold(
-        body: Container(
-          color: Colors.green,
+  @override
+  void initState() {
+    data = [
+      _ChartData('David', 25),
+      _ChartData('Steve', 38),
+      _ChartData('Jack', 34),
+      _ChartData('Others', 52)
+    ];
+    _tooltip = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: SfCircularChart(
+            series: <CircularSeries>[
+              DoughnutSeries<_ChartData, String>(
+                  dataSource: data,
+                  xValueMapper: (_ChartData data, _) => data.x,
+                  yValueMapper: (_ChartData data, _) => data.y,
+                radius: '50%'
+              )
+            ],
+          ),
         ),
       ),
-      debugShowCheckedModeBanner: false,
-    );
+      );
   }
+}
+
+class _ChartData {
+  _ChartData(this.x, this.y);
+
+  final String x;
+  final double y;
 }
