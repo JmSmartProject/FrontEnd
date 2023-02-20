04f2ece3c1fe8900104eb61d5727e0042a6eed65
Jeagu <106376249+JaeKyuP@users.noreply.github.com>
2023-02-20 20:08:36 +0900

    Update pet_signup.dart

--- JMSmart_project/lib/modules/login_page/pet_signup.dart
+++ JMSmart_project/lib/modules/login_page/pet_signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../color/colors.dart';
 
@@ -9,7 +10,7 @@ class PetSignupPage extends StatefulWidget {
 }
 
 class _PetSignupPageState extends State<PetSignupPage> {
-  List<String> Pet_List = ['강아지', '고양이'];
+  List<String> Pet_List = ['강아지','고양이'];
   List<dynamic> petinfo = [];
   String Pet_species = '강아지';
   var pet_male = false;
@@ -92,9 +93,12 @@ class _PetSignupPageState extends State<PetSignupPage> {
                           borderRadius: BorderRadius.circular(15),
                           color: Colors.grey[100],
                         ),
-                        child: Icon(
-                          Icons.camera_alt_outlined,
-                          size: 15,
+                        child: IconButton(
+                          onPressed: () async {
+                            var picker = ImagePicker();
+                            var image = await picker.pickImage(source: ImageSource.gallery);
+                          },
+                          icon: Icon(Icons.camera_alt_outlined),
                         ),
                       ),
                     ),
@@ -200,14 +204,14 @@ class _PetSignupPageState extends State<PetSignupPage> {
                     width: 210,
                     child: TextFormField(
                       style: TextStyle(fontSize: 14),
-                      maxLength: 10,
+                      maxLength: 8,
                       inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.allow(RegExp(r'[0-9|-]')),
+                        FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
                       ],
                       controller: _PetBirthdayController,
                       decoration: InputDecoration(
                         counterText: "",
-                        hintText: "생년월일을 입력해주세요(10자리)",
+                        hintText: "생년월일을 입력해주세요(0000-00-00)",
                         contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
                         hintStyle: TextStyle(fontSize: 14,color: Colors.grey.shade800),
                         enabledBorder: OutlineInputBorder(
@@ -226,7 +230,7 @@ class _PetSignupPageState extends State<PetSignupPage> {
                             petbirthvalidate = 1;
                             _PetBirthValidate.text = '      생년월일을 입력해주세요';
                           }
-                          else if(_PetBirthdayController.text.length <= 9 ){
+                          else if(_PetBirthdayController.text.length <= 7 ){
                             petbirthvalidate = 2;
                             _PetBirthValidate.text = '      8자리가 아닙니다';
                           }
@@ -526,21 +530,21 @@ class _PetSignupPageState extends State<PetSignupPage> {
                               });
                               petinfo.add(_PetNameController.text);
                               petinfo.add(Pet_species);
-                              petinfo.add(_PetBirthdayController.text.toString());  //형
-                              petinfo.add(int.parse(_PetWeightController.text));
-                              petinfo.add(_PetNumberController.text.toString()); //형
+                              petinfo.add(_PetBirthdayController.text);
                               if(Pet_Gender == 1) {
-                                petinfo.add("남");
+                                petinfo.add(1);
                               }
                               else{
-                                petinfo.add("여");
+                                petinfo.add(0);
                               }
+                              petinfo.add(_PetWeightController.text);
                               if(Pet_neutered == 1) {
-                                petinfo.add("NEUTERED");
+                                petinfo.add(1);
                               }
                               else{
-                                petinfo.add("NORMAL");
+                                petinfo.add(0);
                               }
+                              petinfo.add(_PetNumberController.text);
                               if(petvalidate == 0  && Pet_Gender !=2 && Pet_neutered !=2) {
                                 Navigator.pop(context, petinfo);
                               }
